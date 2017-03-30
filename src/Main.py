9fdda1b91d16f8c1f9fdb283a0ea865eec7fa3e5
Gabriele Maurina <gabrielemaurina95@gmail.com>
2017-03-30 17:27:16 +0200

    mysql added

---
+++ src/Main.py
@@ -0,0 +1,10 @@
+print("test")
+
+
+#mysql test
+import mysql.connector
+
+cnx = mysql.connector.connect(user='scott', password='tiger',
+                              host='127.0.0.1',
+                              database='employees')
+cnx.close()
