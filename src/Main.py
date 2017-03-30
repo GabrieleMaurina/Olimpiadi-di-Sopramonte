9b4abede728a9deb6fb2e82df368d39690cc9b0b
Gabriele Maurina <gabrielemaurina95@gmail.com>
2017-03-30 20:30:19 +0200

    mysql query working

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,25 @@
-print("test")
-
-
 #mysql test
 import mysql.connector
 
-cnx = mysql.connector.connect(user='scott', password='tiger',
-                              host='127.0.0.1',
-                              database='employees')
+
+print("before")
+
+cnx = mysql.connector.connect(user="sql11166605", password="FctpTVkSmB",
+                              host="sql11.freemysqlhosting.net",
+                              database="sql11166605", port="3306")
+cursor = cnx.cursor()
+
+cursor.execute("insert into CATEGORY (name, min_age, max_age) values (\"asdf\", \"1\", \"1\")")
+
+
+
+query = ("SELECT CATEGORY_ID, NAME, MIN_AGE, MAX_AGE FROM CATEGORY")
+
+cursor.execute(query)
+
+for (id, name, min, max) in cursor:
+    print("hello",id, name, min, max)
+
 cnx.close()
+
+print("after")
