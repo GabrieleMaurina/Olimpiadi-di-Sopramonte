33c15790286e83647863a9fc1b8b5fc9385b32ba
nicolopomini <nicolo.pomini@studenti.unitn.it>
2017-03-31 22:23:18 +0200

    Primo commit della pagina per inserire un atleta
    Controlla se i campi sono vuoti
    La data va inserita come stringa e in caso convertita in seguito
    Manca tutta la parte che disponga in maniera decente le componenti

---
+++ src/register_athlete.py
@@ -0,0 +1,80 @@
+from tkinter import *
+#Author: Pomo
+
+class RegisterAthlete:
+	def __init__(self, parent):
+		self.nome = ""
+		self.cognome = ""
+		self.nascita = ""
+		self.gender = ""
+			#frame esterno che contiene tutto
+		self.container = Frame(parent) 
+		self.container.pack()
+			#nome
+		self.name_frame = Frame(self.container)
+		self.name_frame.pack()
+		self.name_label = Label(self.name_frame, text = "Nome")
+		self.name_label.pack(side = LEFT)
+		self.name_input = Text(self.name_frame)
+		self.name_input["height"] = 1
+		#self.name_input["width"] = 40
+		self.name_input.config(highlightbackground="black")
+		self.name_input.pack(side = LEFT)
+			#cognome
+		self.sur_frame = Frame(self.container)
+		self.sur_frame.pack()
+		self.sur_label = Label(self.sur_frame, text = "Cognome")
+		self.sur_label.pack(side = LEFT)
+		self.sur_input = Text(self.sur_frame)
+		self.sur_input["height"] = 1
+		#self.sur_input["width"] = 40
+		self.sur_input.config(highlightbackground="black")
+		self.sur_input.pack(side = LEFT)
+			#data di nascita
+		self.date_frame = Frame(self.container)
+		self.date_frame.pack()
+		self.date_label = Label(self.date_frame, text = "Data di nascita")
+		self.date_label.pack(side = LEFT)
+		self.date_input = Text(self.date_frame)
+		self.date_input["height"] = 1
+		#self.date_input["width"] = 10
+		self.date_input.config(highlightbackground="black")
+		self.date_input.pack(side = LEFT)
+			#gender
+		self.gender_frame = Frame(self.container)
+		self.gender_frame.pack()
+		self.gender_label = Label(self.gender_frame, text = "Sesso")
+		self.gender_label.pack(side = LEFT)
+		self.gender_type = StringVar()
+		self.gender_type.set("Maschio")
+		self.gender_choice = OptionMenu(self.gender_frame, self.gender_type, "Femmina")
+		self.gender_choice.pack(side = LEFT)
+			#errors
+		self.error_label = Label(self.container)
+		self.error_label["height"] = 3
+		self.error_label.pack()
+			#button
+		self.confirm_button = Button(self.container)
+		self.confirm_button["text"] = "Conferma"
+		self.confirm_button.bind("<Button-1>", self.conferma)
+		self.confirm_button.pack()
+
+	def conferma(self, event):
+		errore = ""
+		self.nome = self.name_input.get("1.0","end-1c")
+		self.cognome = self.sur_input.get("1.0","end-1c")
+		self.nascita = self.date_input.get("1.0","end-1c")
+		self.gender = self.gender_type.get()
+		if self.nome is "":
+			errore += "Name is missing\n"
+		if self.cognome is "":
+			errore += "Surname is missing\n"
+		if self.nascita is "":
+			errore += "Date of birth is missing"
+		self.error_label.config(text = errore)
+
+
+#Just to debug
+main = Tk()
+start = RegisterAthlete(main)
+main.mainloop()
