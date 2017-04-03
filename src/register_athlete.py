be73a650f8838c59119f3d944700426d98170478
nicolopomini <nicolo.pomini@studenti.unitn.it>
2017-04-03 09:34:52 +0200

    Add files via upload

--- src/register_athlete.py
+++ src/register_athlete.py
@@ -4,17 +4,16 @@ from tkinter import *
 class RegisterAthlete(Frame):
 	def __init__(self, parent):
 		Frame.__init__(parent)
+		self.pack()
 		self.name = ""
 		self.surname = ""
 		self.birth = ""
 		self.gender = ""
-			#frame esterno che contiene tutto
-		self.container = Frame(parent) 
-		self.container.pack()
+		
 			#nome
-		self.name_frame = Frame(self.container)
+		self.name_frame = Frame(self)
 		self.name_frame.pack()
-		self.name_label = Label(self.name_frame, text = "Nome")
+		self.name_label = Label(self.name_frame, text = "Name")
 		self.name_label.pack(side = LEFT)
 		self.name_input = Text(self.name_frame)
 		self.name_input["height"] = 1
@@ -22,9 +21,9 @@ class RegisterAthlete(Frame):
 		self.name_input.config(highlightbackground="black")
 		self.name_input.pack(side = LEFT)
 			#cognome
-		self.sur_frame = Frame(self.container)
+		self.sur_frame = Frame(self)
 		self.sur_frame.pack()
-		self.sur_label = Label(self.sur_frame, text = "Cognome")
+		self.sur_label = Label(self.sur_frame, text = "Surname")
 		self.sur_label.pack(side = LEFT)
 		self.sur_input = Text(self.sur_frame)
 		self.sur_input["height"] = 1
@@ -32,9 +31,9 @@ class RegisterAthlete(Frame):
 		self.sur_input.config(highlightbackground="black")
 		self.sur_input.pack(side = LEFT)
 			#data di nascita
-		self.date_frame = Frame(self.container)
+		self.date_frame = Frame(self)
 		self.date_frame.pack()
-		self.date_label = Label(self.date_frame, text = "Data di nascita")
+		self.date_label = Label(self.date_frame, text = "Birthday")
 		self.date_label.pack(side = LEFT)
 		self.date_input = Text(self.date_frame)
 		self.date_input["height"] = 1
@@ -42,21 +41,21 @@ class RegisterAthlete(Frame):
 		self.date_input.config(highlightbackground="black")
 		self.date_input.pack(side = LEFT)
 			#gender
-		self.gender_frame = Frame(self.container)
+		self.gender_frame = Frame(self)
 		self.gender_frame.pack()
-		self.gender_label = Label(self.gender_frame, text = "Sesso")
+		self.gender_label = Label(self.gender_frame, text = "Gender")
 		self.gender_label.pack(side = LEFT)
 		self.gender_type = StringVar()
-		self.gender_type.set("Maschio")
-		self.gender_choice = OptionMenu(self.gender_frame, self.gender_type, "Femmina")
+		self.gender_type.set("Male")
+		self.gender_choice = OptionMenu(self.gender_frame, self.gender_type, "Female")
 		self.gender_choice.pack(side = LEFT)
 			#errors
-		self.error_label = Label(self.container)
+		self.error_label = Label(self)
 		self.error_label["height"] = 3
 		self.error_label.pack()
 			#button
-		self.confirm_button = Button(self.container)
-		self.confirm_button["text"] = "Conferma"
+		self.confirm_button = Button(self)
+		self.confirm_button["text"] = "Confirm"
 		self.confirm_button.bind("<Button-1>", self.conferma)
 		self.confirm_button.pack()
 
