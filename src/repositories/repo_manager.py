8067016c64318d2098010ea81da9c4ed3f34e61f
Marvash <marcusmerlin@gmail.com>
2017-04-14 22:53:46 +0200

    Category_repository added

--- src/repositories/repo_manager.py
+++ src/repositories/repo_manager.py
@@ -4,10 +4,14 @@ from domain.athlete import *
 from domain.athlete_competition import *
 from domain.category import *
 from domain.competition import *
+from domain.result import *
+from domain.team import *
 from repositories.athlete_repository import *
 from repositories.athlete_competition_repository import *
 from repositories.category_repository import *
 from repositories.competition_repository import *
+from repositories.result_repository import *
+from repositories.team_repository import *
 
 class RepoManager:
 
@@ -27,6 +31,8 @@ class RepoManager:
         self.athleteCompetitionRepository = AthleteCompetitionRepository(self.cnx)
         self.categoryRepository = CategoryRepository(self.cnx)
         self.competitionRepository = CompetitionRepository(self.cnx)
+        self.resultRepository = ResultRepository(self.cnx)
+        self.teamRepository = TeamRepository(self.cnx)
 
     def __del__(self):
         self.cnx.close()
